4e8bb2c685a1a7ea7fd50dd651782bdc48b16900
Jeremy-Ramirez <66746920+Jeremy-Ramirez@users.noreply.github.com>
2022-11-04 22:53:47 -0500

    Nuevo comit en nueva rama

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <>
-        <h1 className='text-3xl font-black text-center md:w-2/3 mx-auto '>Seguimiento Pacientes {""}
+        <h1 className='text-3xl font-black text-center md:w-2/3 mx-auto '>Seguimiento Pacientes {""} con Github {''}
             <span className='text-blue-400'>Veterinaria</span>
         </h1>
     </>
